c85a3119a5fbbe55e9d910e4bddcd610d8e8f108
Борис Сизов <sizov@Boriss-MacBook-Air.local>
2025-09-22 00:08:36 +0400

    Подключение UIKit в проект и настройка зависимостей

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,8 @@
 console.log('hello from main.js')
+
+// src/js/app.js
+import UIkit from 'uikit';
+import Icons from 'uikit/dist/js/uikit-icons';
+
+// подключаем пакет иконок (офканвас, модалки и т.п. используют его)
+UIkit.use(Icons);
